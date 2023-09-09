3201274f09411aed43f5e876b17b9ef643c4f33d
Anthony <charetteanthony@outlook.com>
2023-09-09 16:02:15 -0400

    Removed Vite and currently using another solution to minify the js files

--- build/src/chatBudy.js
+++
@@ -1,9 +0,0 @@
-fetch('../dist/manifest.json')
-  .then(response => response.json())
-  .then(manifest => {
-    const mainJsPath = manifest['main.js'].file;
-    // Dynamically inject the scripts
-    const script1 = document.createElement('script');
-    script1.src = `.././${mainJsPath}`;
-    document.head.appendChild(script1);
-  });
