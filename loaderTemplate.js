ed16137d3e52fa9369aa421004729ac3acb92976
Anthony <charetteanthony@outlook.com>
2023-09-12 14:40:03 -0400

    Modif

--- loaderTemplate.js
+++ loaderTemplate.js
@@ -106,28 +106,57 @@ function initializeLoader(){
         let useraccess = '{{USER_HASH}}'
         LoadUpsequence(useraccess)
         // set up a new html document
-        const newHTMLDoc = `
-            <!DOCTYPE html>
-                <head>
-                    <script src="https://kit.fontawesome.com/76351f6769.js" crossorigin="anonymous"></script>
-                    <link rel="preconnect" href="https://fonts.googleapis.com">
-                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-                    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;600&display=swap">
-                </head>
-                <body>
-                    <script type="module" src="https://chat-buddy-widget.vercel.app/chatBudy.js" async></script>
-                </body>
-            </html>
-        `;
+        // const newHTMLDoc = `
+        //     <!DOCTYPE html>
+        //         <head>
+        //             <script src="https://kit.fontawesome.com/76351f6769.js" crossorigin="anonymous"></script>
+        //             <link rel="preconnect" href="https://fonts.googleapis.com">
+        //             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        //             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;600&display=swap">
+        //         </head>
+        //         <body>
+        //             <script type="module" src="https://chat-buddy-widget.vercel.app/chatBudy.js" async></script>
+        //         </body>
+        //     </html>
+        // `;
         // set up the Iframe
         let Iframe = document.createElement('iframe');
         Iframe.style.display = "none";
         Iframe.title = "ChatBudy chat widget code"
         Iframe.onload = () => {
-            const IframeDocument = Iframe.contentDocument;
-            IframeDocument.open();
-            IframeDocument.documentElement.replaceWith(newHTMLDoc);
-            IframeDocument.close();
+            // Create a new document within the iframe
+            const iframeDocument = Iframe.contentDocument || Iframe.contentWindow.document;
+
+            // Create HTML content using DOM manipulation
+            const newHTMLDoc = iframeDocument.createElement("html");
+            const head = iframeDocument.createElement("head");
+            const body = iframeDocument.createElement("body");
+
+            // Create and append elements to the head and body
+            const scriptElement = iframeDocument.createElement("script");
+            scriptElement.src = "https://kit.fontawesome.com/76351f6769.js";
+            scriptElement.crossOrigin = "anonymous";
+            head.appendChild(scriptElement);
+
+            const linkElement = iframeDocument.createElement("link");
+            linkElement.rel = "stylesheet";
+            linkElement.href = "https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;600&display=swap";
+            head.appendChild(linkElement);
+
+            const script2 = iframeDocument.createElement("script");
+            script2.type = "module";
+            script2.src = "https://chat-buddy-widget.vercel.app/chatBudy.js";
+            script2.async = true;
+            body.appendChild(script2);
+
+            // Append head and body to the HTML document
+            newHTMLDoc.appendChild(head);
+            newHTMLDoc.appendChild(body);
+
+            // Replace the entire content of the iframe's document
+            iframeDocument.open();
+            iframeDocument.replaceChild(newHTMLDoc, iframeDocument.documentElement);
+            iframeDocument.close();
         }
         // append both of them together
         document.body.appendChild(Iframe)
