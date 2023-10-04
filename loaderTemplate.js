341d823147ddc22b81d939d33bc711daea1fd757
Anthony <charetteanthony@outlook.com>
2023-10-04 14:30:43 -0400

    Modif

--- loaderTemplate.js
+++ loaderTemplate.js
@@ -126,6 +126,10 @@ const SetupIframe = async (Iframe_element) => {
         Iframe_element.setAttribute('srcdoc', initialHTML);
         // Append the iframe to the body
         document.body.appendChild(Iframe_element);
+        const parentScriptExists = Array.from(document.head.getElementsByTagName('script')).some((script) => {
+            return script.src === 'https://kit.fontawesome.com/76351f6769.js';
+        });
+        console.log('existe', parentScriptExists)
         // Wait for the iframe to load
         Iframe_element.onload = async () => {
             // You can now manipulate the content of the iframe if needed
@@ -135,16 +139,10 @@ const SetupIframe = async (Iframe_element) => {
                  * Setting up Google Fonts & Font awesome for the widget
                  */
                 // <script src="https://kit.fontawesome.com/76351f6769.js" crossorigin="anonymous"></script>
-                // const parentScriptExists = Array.from(document.head.getElementsByTagName('script')).some((script) => {
-                //     return script.src === 'https://kit.fontawesome.com/76351f6769.js';
-                // });
-                // console.log('existe', parentScriptExists)
-                // if(!parentScriptExists){
-                //     // const iframe_head_script = iframeDocument.createElement("script")
-                //     // iframe_head_script.src = "https://kit.fontawesome.com/76351f6769.js";
-                //     // iframe_head_script.crossOrigin = "anonymous";
-                //     // iframeDocument.head.appendChild(iframe_head_script)
-                // }
+                    // const iframe_head_script = iframeDocument.createElement("script")
+                    // iframe_head_script.src = "https://kit.fontawesome.com/76351f6769.js";
+                    // iframe_head_script.crossOrigin = "anonymous";
+                    // iframeDocument.head.appendChild(iframe_head_script)
                 // <link rel="preconnect" href="https://fonts.googleapis.com">
                 const iframe_link1 = iframeDocument.createElement("link")
                 iframe_link1.rel = "preconnect";
