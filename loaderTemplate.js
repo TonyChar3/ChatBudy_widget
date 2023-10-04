aef288a4fb6ca38fce2838445332bfa6fcb0861f
Anthony <charetteanthony@outlook.com>
2023-10-04 15:12:25 -0400

    Modif

--- loaderTemplate.js
+++ loaderTemplate.js
@@ -170,6 +170,10 @@ export const initiateChat = async(widget_id) => {
 export const LoadUpsequence = async(widget_id) => {
     try{
         if (sessionStorage.getItem('widgetLoaded') || sessionStorage.getItem('convoClosed')) {
+            // Create the iframe element with srcdoc
+            const Iframe = document.createElement('iframe');
+            // Setting up the Iframe in the document
+            SetupIframe(Iframe);
             return
         }
         const response = await fetch(`http://localhost:8080/visitor/visitor-info`,{
@@ -188,6 +192,10 @@ export const LoadUpsequence = async(widget_id) => {
                 localStorage.setItem('chatbudy_state', state_obj)
             }
             await GetWidgetStyle(widget_id);
+            // Create the iframe element with srcdoc
+            const Iframe = document.createElement('iframe');
+            // Setting up the Iframe in the document
+            SetupIframe(Iframe);
         }
     } catch(err){
         console.log('Load up sequence ERROR: ', err)
@@ -195,10 +203,6 @@ export const LoadUpsequence = async(widget_id) => {
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Create the iframe element with srcdoc
-    const Iframe = document.createElement('iframe');
-    // Setting up the Iframe in the document
-    SetupIframe(Iframe);
    // load visitor chat session + his info
    LoadUpsequence(useraccess);
 });
