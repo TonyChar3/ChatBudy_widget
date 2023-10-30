baffc4b9cc110f9025e8a1f7decd8da99c69358e
Anthony <charetteanthony@outlook.com>
2023-10-30 08:55:00 -0400

    Modif

--- asset.js
+++ asset.js
@@ -493,7 +493,8 @@ export const openChat = async(widget_id, sse_connection) => {
             if(!sessionStorage.getItem('convoClosed') && sessionStorage.getItem('widgetLoaded')){
                 // will send the user_hash and the httpOnly cookie jwt 
                 //TODO: credentials: 'include' once in production to send the httpOnly cookie
-                const token = getCookie('visitor_jwt');
+                //const token = getCookie('visitor_jwt');
+                const token = 'helo'
                 const ws_auth_fetch = await fetch('http://localhost:8080/chat/auth-ws',{
                     method: 'post',
                     headers: {
