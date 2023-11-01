ecb7cbf907c3018fb434a92e5e67310c85a8c6ba
Anthony <charetteanthony@outlook.com>
2023-11-01 13:45:36 -0400

    fix wrong domain name for the api

--- loaderTemplate.js
+++ loaderTemplate.js
@@ -189,10 +189,8 @@ export const LoadUpsequence = async(widget_id) => {
 document.addEventListener('DOMContentLoaded', async() => {
     // load visitor chat session + his info
     LoadUpsequence(useraccess);
-    if(sessionStorage.getItem('widgetLoaded')){
-        // Create the iframe element with srcdoc
-        const Iframe = document.createElement('iframe');
-        // Setting up the Iframe in the document
-        SetupIframe(Iframe);
-    }
+    // Create the iframe element with srcdoc
+    const Iframe = document.createElement('iframe');
+    // Setting up the Iframe in the document
+    SetupIframe(Iframe);
 });
