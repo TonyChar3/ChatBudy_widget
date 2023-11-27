74a17a62985dbd723e34b110c14f8b1df90cc887
Anthony <charetteanthony@outlook.com>
2023-11-27 08:38:47 -0500

    FIX sse notification not clearing

--- main.js
+++ main.js
@@ -625,9 +625,7 @@ class SalezyWidget {
       }
       this.widgetContainer.style.zIndex = 30
       this.buttonContainer.style.zIndex = 50
-      console.log('clsing', this.SSElink)
-      this.SSElink !== null ? this.SSElink.close() : '';// Shut off the SSE connection for the notifications
-      console.log(this.SSElink)
+      this.SSElink !== null ? console.log(this.SSElink.close()) : '';// Shut off the SSE connection for the notifications
       this.WebSocketHandler(this.widgetID);
       this.widgetIcon.classList.add("widget__hidden");
       this.sendIcon.classList.remove("widget__hidden");
